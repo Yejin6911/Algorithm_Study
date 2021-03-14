c6712e538cbfb365910034093f2f797981a9d280
mjson1954 <mjson1954@gmail.com>
2021-03-14 13:33:36 +0900

    20210315 minjoo 1744 푸는중.....

---
+++ minjoo/greedy/1744.py
@@ -0,0 +1,20 @@
+n = int(input()) # 수열의 크기
+
+plus = []
+minus = []
+
+for _ in range(n):
+    num = int(input())
+    if(num > 0):
+        plus.append(num)
+    else:
+        minus.append(num)
+
+
+
+
+
+
+
+
+# print(answer)

feda57a55d6063f141666833b60ac1fcfa968a20
mjson1954 <mjson1954@gmail.com>
2021-03-23 15:27:12 +0900

    20210323

--- minjoo/greedy/2812.py
+++ minjoo/greedy/2812.py
@@ -4,16 +4,16 @@ n, k = map(int, input().split())
 num = list(input())
 
 cnt = 0
-stack = []
-answer = ''
+stack = [] # 정답
 
 for i in range(n):
     while(stack and stack[-1] < num[i] and cnt < k):
+        # stack의 마지막 원소가 더 작으면 삭제
         stack.pop()
         cnt += 1
     stack.append(num[i])
 
-while(cnt < k):
+while(cnt < k): # 삭제할 횟수가 남았을 때
     stack.pop()
     cnt += 1
 

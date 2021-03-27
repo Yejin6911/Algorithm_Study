5f4852e722c0f16fef5ecab1d938ec3afdb8614a
Unknown <cdjin6911@gmail.com>
2021-03-27 15:16:53 +0900

    20210327 yejin 14499

--- yejin/삼성 SW 역량 테스트 기출 문제/13458.py
+++ yejin/삼성 SW 역량 테스트 기출 문제/13458.py
@@ -4,7 +4,9 @@ N = int(sys.stdin.readline().rstrip())
 data = list(map(int, sys.stdin.readline().rstrip().split()))
 B, C = map(int, sys.stdin.readline().rstrip().split())
 
-cnt = N
+cnt = N  # 모든방의 총감독관 배치
+
+# 부감독관 배치
 for i in range(N):
     if data[i] - B >= 0:
         cnt += (data[i]-B)//C

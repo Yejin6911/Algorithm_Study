3fc5317b84966eff9ab6680c8a695b19c6282af6
mjson1954 <mjson1954@gmail.com>
2021-07-25 12:55:56 +0900

    20210725

--- minjoo/DFS/2644.py
+++ minjoo/DFS/2644.py
@@ -1,3 +1,5 @@
+import sys
+
 n = int(input()) # 전체 사람의 수
 a, b = map(int, input().split())
 m = int(input()) # 관계의 개수
@@ -13,23 +15,34 @@ for _ in range(m):
         dic[y].append(x)
     else:
         dic[y] = [x]
-
 print(dic)
 visited = [0 for _ in range(n+1)]
-ans = 0
-
-def dfs(a, b):
-    global visited, ans
+flag = 0
+answer = sys.maxsize
+def dfs(a, b, ans):
+    global visited, flag, answer
     visited[a] = 1
+    if(a == b):
+        if(answer > ans):
+            answer = ans
+            return
     for i in range(len(dic[a])):
         if(dic[a][i] == b):
             ans += 1
-            return ans
+            flag = 1
+            if(answer > ans):
+                answer = ans
+                return
         if(dic[a][i] in dic.keys() and visited[dic[a][i]] == 0):
             ans += 1
-            dfs(dic[a][i], b)
-    return -1
+            dfs(dic[a][i], b, ans)
 
-print(dfs(a, b))
+
+dfs(a, b, 0)
+print(visited)
+if(flag == 1):
+    print(answer)
+else:
+    print("-1")
 
 # 푸는중

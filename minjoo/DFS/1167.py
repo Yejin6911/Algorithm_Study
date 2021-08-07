9db89bb31869d88ec0bd8d6fdeaee6a99c5210a0
mjson1954 <mjson1954@gmail.com>
2021-08-07 13:32:30 +0900

    20210807

--- minjoo/DFS/1167.py
+++ minjoo/DFS/1167.py
@@ -6,16 +6,17 @@
 import sys
 sys.setrecursionlimit(10**9)
 
-n = int(input())
-tree = [[] for _ in range(n+1)]
-for _ in range(n-1):
-    x, y, length = map(int, input().split())
-    tree[x].append([y, length])
-    tree[y].append([x, length])
+v = int(input())
+tree = [[] for _ in range(v+1)]
 
-# print(tree)
+for _ in range(v):
+    temp = list(map(int, input().split()))
+    for i in range(1, len(temp)-1, 2):
+        tree[temp[0]].append([temp[i], temp[i+1]])
+        tree[temp[i]].append([temp[0], temp[i+1]])
 
-result1 = [0 for _ in range(n+1)]
+# 첫번째 dfs 결과
+result1 = [0 for _ in range(v+1)]
 
 def dfs(start, tree, result):
     for y, length in tree[start]:
@@ -23,18 +24,19 @@ def dfs(start, tree, result):
             result[y] = result[start] + length
             dfs(y, tree, result)
 
-dfs(1, tree, result1)
-result1[1] = 0
+dfs(1, tree, result1) # 아무노드에서 시작
+result1[1] = 0 
 
-tmpmax = 0
-index = 0
+tmpmax = 0 # 최대값 구하기
+index = 0 # 최장경로 노드
 
 for i in range(len(result1)):
     if(tmpmax < result1[i]):
         tmpmax = result1[i]
         index = i
 
-result2 = [0 for _ in range(n+1)]
+# 최장경로 노드에서 다시 dfs를 통해 트리지름 구하기
+result2 = [0 for _ in range(v+1)]
 dfs(index, tree, result2)
 result2[index] = 0
 print(max(result2))

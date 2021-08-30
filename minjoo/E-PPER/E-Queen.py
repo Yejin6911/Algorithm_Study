ec2b3481d1ebe78c83ad259d04fd4e0e8092e5ef
mjson1954 <mjson1954@gmail.com>
2021-08-30 11:32:18 +0900

    20210830 e-queen

--- minjoo/E-PPER/E-Queen.py
+++ minjoo/E-PPER/E-Queen.py
@@ -12,9 +12,11 @@ col = [0] * (n+1)
 
 def promising(i, col):
     for k in range(1, i):
-        if(col[i] == col[k] or (abs(col[i]-col[k]) == i-k)):
+        if(col[i] == col[k] or (abs(col[i]-col[k]) == abs(i-k))):
             return False
         if([k, col[k]] in pos):
+            return False
+        if([i, col[i]] in pos):
             return False
     return True
 

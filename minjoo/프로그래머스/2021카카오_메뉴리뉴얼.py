a05245ead44d81eed0c5c31545e217e115bd1cdf
mjson1954 <mjson1954@gmail.com>
2021-09-04 13:29:36 +0900

    20210904

--- minjoo/프로그래머스/2021카카오_메뉴리뉴얼.py
+++ minjoo/프로그래머스/2021카카오_메뉴리뉴얼.py
@@ -1,6 +1,4 @@
 from itertools import combinations
-import copy
-import sys
 
 def solution(orders, course):
     answer = []
@@ -51,6 +49,6 @@ def solution(orders, course):
         ans.append(t)
     return ans
 
-print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4] ))
-print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"],[2,3,5] ))
-print(solution(["XYZ", "XWY", "WXA"],[2,3,4] ))
+# print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4] ))
+# print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"],[2,3,5] ))
+# print(solution(["XYZ", "XWY", "WXA"],[2,3,4] ))
